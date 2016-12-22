4a43881eb42657c59d3f6da7fad70e8d7bc6af17
Richard Bata <rb10g13@ecs.soton.ac.uk>
2016-12-22 18:39:11 +0100

    Server small changes

--- CochlearMicSignal/server/mongoose/models/Reading.js
+++ CochlearMicSignal/server/mongoose/models/Reading.js
@@ -4,22 +4,13 @@ console.log('Here');
 
 var ReadingSchema = new mongoose.Schema({
   // Core content data
-  title: String,
-  // location: {
-  //   // North-South
-  //   lat: { type: Number, min: -90, max: 90 },
-  //   // East-West
-  //   long: { type: Number, min: -180, max: 180 }
-  // },
-  // abstract: String,
-  // content: String, //TODO
-  // images: [String],
-
-  // // Social
-  // social: {
-  //   likes: [String], // Usernames
-  //   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
-  // }
+  implant_id: {
+    number: Number,
+    ear: String
+  },
+  readings: [
+    [Number][Number]
+  ]
 });
 
 mongoose.model('Reading', ReadingSchema);
